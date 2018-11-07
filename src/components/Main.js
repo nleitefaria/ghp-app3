02287451144fec4af346c0b38561859ae6990681
nleitefaria <nleitefaria@gmail.com>
2018-11-07 22:14:10 +0000

    Added Header

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,35 +6,44 @@ import {
 }
 from "react-router-dom";
 
+import 
+{
+	  Container, Row, Col
+} 
+from 'reactstrap';
+
 import Home from './Home';
 import Employees from './Employees';
 import Projects from './Projects';
+import Header from './Header';
 
 class Main extends Component
 {
 	  render() {
-	    return (
-	    		<HashRouter>
+	    return (					
+				<HashRouter>				
 	            	<div>
 	            		<div>
-	            			
+							<Container>
+	            				<Row>
+	            					<Col>
+										<Header></Header>
 	            						<ul className="header">
 	            							<li><NavLink to="/">Home</NavLink></li>
 	            							<li><NavLink to="/employees">Employees</NavLink></li>
 	            							<li><NavLink to="/projects">Projects</NavLink></li>
-
 	            						</ul>
-
 	            						<div className="content">
 	            							<Route exact path="/" component={Home}/>
 	            							<Route path="/employees" component={Employees}/>
 	            							<Route path="/projects" component={Projects}/>
-
 	            						</div>
-	            					
+									</Col>
+	            				</Row>
+	            			</Container>            					
 	            		</div>
 	            	</div>
-	            </HashRouter>
+	            </HashRouter>								
 	    );
 	  }
 }
