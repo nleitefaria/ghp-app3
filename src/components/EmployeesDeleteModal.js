6b0b97b90927b6973ed4bebbb8e0de6530684edc
nleitefaria <nleitefaria@gmail.com>
2018-11-13 20:22:48 +0000

    clean

--- src/components/EmployeesDeleteModal.js
+++ src/components/EmployeesDeleteModal.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+const apiURL = 'https://sec-os-app3.7e14.starter-us-west-2.openshiftapps.com/'
+
 class EmployeesDeleteModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      name:''
     };
 
     this.toggle = this.toggle.bind(this);
   }
 
   toggle() {
+    if(this.state.modal === false)
+    {
+      this.loadData(this.props.id);
+    }
+
     this.setState({
       modal: !this.state.modal
     });
+  }
+
+  loadData(id)
+  {
+    axios.get(apiURL + 'employee/' + id).then(res =>
+    {
+      const rd = res.data;
+      this.setState( {name: rd.name} );
+    })
+
   }
 
   render() {
@@ -24,7 +42,7 @@ class EmployeesDeleteModal extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Delete Employee</ModalHeader>
           <ModalBody>
-            Are you sure you want to delete employee NAME <br></br>
+            Are you sure you want to delete employee {this.state.name} <br></br>
             and his allocation to projects?
           </ModalBody>
           <ModalFooter>
