a6d029c9eff68df57cd91d2f6408c7d222aeb370
nf-faria <nf-faria@PT-PF0YBD7V.critical.pt>
2018-11-14 18:28:20 +0000

    clean

---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Todos extends React.Component
+{
+   render() {
+      return (
+         <div>
+            <br></br>
+            Todos
+
+         </div>
+      );
+   }
+}
+
+export default Todos;
