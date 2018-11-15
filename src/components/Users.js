2e9c22ac7e3dd90fded2e7e7f740517f786d6eb9
nf-faria <nf-faria@PT-PF0YBD7V.critical.pt>
2018-11-15 16:20:57 +0000

    Added context

---
+++ src/components/Users.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { OtherConsumer } from "./OtherContext";
+
+class Users extends React.Component
+{
+  render() {
+    return (
+      <div>
+        <br></br>
+        <OtherConsumer>
+        {
+          context =>
+            <React.Fragment>
+              <p><b>First name:</b> {context.firstName}</p>
+              <p><b>Last name:</b> {context.lastName}</p>
+            </React.Fragment>
+        }
+        </OtherConsumer>
+      </div>
+    );
+  }
+}
+
+export default Users;
---
+++ src/components/UsersContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UsersContent extends React.Component
+{
+   render() {
+      return (
+         <div>
+            <br></br>
+            Users Content
+            <br></br>
+
+         </div>
+      );
+   }
+}
+
+export default UsersContent;
