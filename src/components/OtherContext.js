d963297ce792e6b7668c997a0ed3316bcb8f337a
nf-faria <nf-faria@PT-PF0YBD7V.critical.pt>
2018-11-15 18:13:35 +0000

    clean

--- src/components/OtherContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const OtherContext = React.createContext({});
-
-export const OtherProvider = OtherContext.Provider;
-export const OtherConsumer = OtherContext.Consumer;
