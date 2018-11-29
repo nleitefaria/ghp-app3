ee4233417026d39a4d328cfa4502156d5ca47427
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-29 19:28:44 +0000

    Deploy nleitefaria/ghp-app3 to github.com/nleitefaria/ghp-app3.git:gh-pages

---
+++ precache-manifest.d0ffbc9f8381518558dc5cc2b473553c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5026e454929b264e12",
+    "url": "/ghp-app3/static/css/main.19d4fc5d.chunk.css"
+  },
+  {
+    "revision": "cd5026e454929b264e12",
+    "url": "/ghp-app3/static/js/main.cd5026e4.chunk.js"
+  },
+  {
+    "revision": "6a0e3b7949423abdb763",
+    "url": "/ghp-app3/static/css/1.1d2c9208.chunk.css"
+  },
+  {
+    "revision": "6a0e3b7949423abdb763",
+    "url": "/ghp-app3/static/js/1.6a0e3b79.chunk.js"
+  },
+  {
+    "revision": "003924bc7d2b774a5f90",
+    "url": "/ghp-app3/static/js/runtime~main.003924bc.js"
+  },
+  {
+    "revision": "51b556449464a70556c30fdd7c1809b7",
+    "url": "/ghp-app3/index.html"
+  }
+];
